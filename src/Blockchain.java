c6332ff26f07f57524e19d32b6a60995f78c70a8
aponda <aponda@null>
2021-03-03 13:56:41 -0600

    Updated 3/4

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -57,6 +57,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class Blockchain {
     private int serverMode = 0;
@@ -113,15 +114,23 @@ public class Blockchain {
         }
 
         bce.startListeners();
+
+        //Sleep for five seconds to wait for the other peers to start up.
+        //todo: implement a node registration system for the peers. This is currently static.
         try{ Thread.sleep(5000);} catch(InterruptedException e)
         {
             System.out.println("Interrupted: " + e.getMessage());
             Thread.currentThread().interrupt();
         }
 
+        //First share public key with peers.
+        bce.broadcastPublicKey();
+
+
+        //then begin processing my local database.
+        bce.startUVBProcessor();
         bce.processLocalPatientLedger();
 
-        bce.broadcastPublicKey();
         /*
         // Print all of the providers supported on this system.
         for(int i = 0; i<Security.getProviders().length; i++) {
@@ -129,7 +138,6 @@ public class Blockchain {
         }
         */
 
-        //final PriorityBlockingQueue<BlockRecord> ourPriorityQueue = new PriorityBlockingQueue<>(100);
     }
 
 }
@@ -153,12 +161,14 @@ class EncodedPubKeyStruct {
 }
 
 class EncodedUVBStruct {
-    String encodedUVB;
+    String jsonUVB;
+    String encodedSig;
     String pid;
 
-    EncodedUVBStruct(String encodedUVB, int id){
-        this.encodedUVB = encodedUVB;
+    EncodedUVBStruct(String jsonUVB, String encodedSig, int id){
+        this.jsonUVB = jsonUVB;
         this.pid = Integer.toString(id);
+        this.encodedSig = encodedSig;
     }
 }
 
@@ -180,25 +190,40 @@ class BCstruct{
         return recordList.getFirst();
     }
 
-    public void addRecord(BlockRecord br){
+    public boolean addRecord(BlockRecord br){
 
         // If this is the first block.
         if(this.recordList.size() == 0) {
             this.recordList.addFirst(br);
+            return true;
         } else {
             // Check that prevHash matches
             if (br.previousHash.equals(this.getLastestBlock().createBlockRecordHash())){
-                System.out.println("SUCCESS ADDING BLOCK");
+                //System.out.println("SUCCESS ADDING BLOCK");
                 this.recordList.addFirst(br);
+                return true;
             } else {
-                System.out.println("FAILED ADDING BLOCK");
+                //System.out.println("FAILED ADDING BLOCK");
                 System.out.print(br.previousHash);
                 System.out.print(" != ");
                 System.out.print(this.getLastestBlock().createBlockRecordHash());
                 System.out.print("\n");
-
+                return false;
             }
         }
+    }
+
+    public boolean findBLockID(String uuidcheck){
+        for(int i = 0; i < this.recordList.size(); i++){
+            if(uuidcheck.equals(this.recordList.get(i).getBlockID())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkPreviousHashMatch(BlockRecord br){
+        return br.previousHash.equals(this.getLastestBlock().createBlockRecordHash());
     }
 
     public void printBC(){
@@ -217,8 +242,28 @@ class BCstruct{
     }
 
 }
+
+class BlockRecordComparator implements Comparator<BlockRecord>{
+    @Override
+    public int compare(BlockRecord a, BlockRecord b){
+        DateFormat format;
+        Date aa;
+        Date bb;
+
+        try {
+            format = new SimpleDateFormat(a.DATEFORMAT, Locale.ENGLISH);
+            aa = format.parse(a.timestamp);
+            bb = format.parse(b.timestamp);
+        } catch (IllegalArgumentException | ParseException e ){
+            System.out.println("Comparator error - format given must be '" + a.DATEFORMAT + "'");
+            throw new IllegalArgumentException(e.getMessage());
+        }
+
+        return aa.compareTo(bb);
+    }
+}
 class BlockRecord implements Serializable{
-    private final String DATEFORMAT = "yyyy-MM-dd.HH:mm:ss.S";
+    final String DATEFORMAT = "yyyy-MM-dd.HH:mm:ss.S";
     private final int NONCE_LEN = 2; //in bytes
     private final String HASH_TYPE = "SHA-256";
 
@@ -667,6 +712,7 @@ class SecurityHelper{
 // Checks inital connection parameters and tracks a connection counter.
 class BCExecutor{
     public static final int VERBOSE = 1;  // if verbose = 1 server will print more output
+    public final int difficulty = 21;
 
     int keyport;
     int UVBport;
@@ -681,6 +727,9 @@ class BCExecutor{
     // Initialize a basic neighbor structure.
     BCPeer[] neighs; // the list of neighbors
     KeyPair mykey; // the private/public keypair for this server.
+
+    final PriorityBlockingQueue<BlockRecord> UVBqueue = new PriorityBlockingQueue<>(100, new BlockRecordComparator());;
+
 
     // Constructor: Initialize the server socket.
     BCExecutor(int basekeyport, int baseUBport, int baseBCport, int q_len, int pid) throws NoSuchProviderException, NoSuchAlgorithmException {
@@ -747,7 +796,7 @@ class BCExecutor{
         while(intcomp != 0) {
             random.nextBytes(nonce);
             newblock.setNonce(BlockRecord.byteToHexString(nonce));
-            newblock.resetTimestamp();
+            //newblock.resetTimestamp();
 
             hash = newblock.createBlockRecordHash();
             intcomp = Long.parseLong(hash.substring(0,15), 16);
@@ -805,9 +854,15 @@ class BCExecutor{
 
     }
 
-    void processLocalPatientLedger(){
-        int difficulty = 21;
+    void startUVBProcessor(){
+        try {
+            new Thread(new QueueProcessor(this)).start();
+        } catch (IOException e){
+            System.out.println("ERROR: Failed to start Processor");
+        }
+    }
 
+    void processLocalPatientLedger(){
         String[] records;
         try {
             records = IOHelper.readRecordsFile(Lfilename);
@@ -817,20 +872,48 @@ class BCExecutor{
             return;
         }
 
-        for (int i = 0; i < records.length; i++){
+        int i = 0;
+        while (i < records.length){
             //System.out.println(records[i]);
 
             String prevHash = BC.getLastestBlock().createBlockRecordHash();
             //System.out.println( BC.getLastestBlock().getBlockID());
             BlockRecord newblock = new BlockRecord(records[i], prevHash, pid );
 
-            System.out.println(doWork2(difficulty, newblock, false));
+            //System.out.println(doWork2(difficulty, newblock, false));
             //System.out.println(newblock.createBlockRecordHash());
             //System.out.println("VERIFYING WORK: " + verifyWork2(difficulty, newblock));
+            try {
+                broadcastUVB(newblock);
+            } catch (Exception e){
+                System.out.println("Error: failed to broadcast new UVB block");
+                e.printStackTrace();
+            }
 
-            BC.addRecord(newblock);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted: " + e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+
+            if(BC.findBLockID(newblock.getBlockID())){
+                //retry the record
+                i++;
+            }
+            //BC.addRecord(newblock);
             //myBC.printBC();
         }
+
+        /*
+        BlockRecord br;
+        while((br = this.UVBqueue.poll()) != null){
+            System.out.println(doWork2(difficulty, br, false));
+            BC.addRecord(br);
+
+        }
+
+         */
 
         //String bcjsonstring = IOHelper.getJSONFromObject(BC);
         //System.out.println(bcjsonstring);
@@ -846,8 +929,7 @@ class BCExecutor{
         this.BC.printBC();
     }
 
-    //This is just a test function, not used for the real program.
-    void broadcastBC() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchProviderException, SignatureException {
+    void broadcastBC(){
         Socket sock;
         PrintStream out;
 
@@ -856,6 +938,25 @@ class BCExecutor{
         //SecurityHelper.signData(bcjsonstring.getBytes(StandardCharsets.UTF_8), this.mykey.getPrivate());
 
         broadcastHelper(bcjsonstring, 1);
+
+    }
+
+    void broadcastUVB(BlockRecord newblock) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchProviderException, SignatureException {
+        Socket sock;
+        PrintStream out;
+
+        //SecurityHelper.verifySignedData(pubket, sig, data);
+
+        String jsonUVB = IOHelper.getJSONFromObject(newblock);
+        byte[] sig = SecurityHelper.signData(jsonUVB.getBytes(StandardCharsets.UTF_8), this.mykey.getPrivate());
+        String base64sig = SecurityHelper.base64EncodeBytes(sig);
+
+        EncodedUVBStruct encUVB = new EncodedUVBStruct(jsonUVB, base64sig, this.pid);
+
+        String jsonMessage = IOHelper.getJSONFromObject(encUVB);
+        //SecurityHelper.signData(bcjsonstring.getBytes(StandardCharsets.UTF_8), this.mykey.getPrivate());
+
+        broadcastHelper(jsonMessage, 2);
 
     }
 
@@ -915,6 +1016,44 @@ class BCExecutor{
             }
         }
     }
+    class QueueProcessor implements Runnable{
+        // listen for a new connection. If one is received hand of the socket
+        // to a new thread in WebWorker.
+        BCExecutor bce;
+
+        QueueProcessor(BCExecutor bce) throws IOException{
+            this.bce = bce;
+        }
+
+        public void listen(){
+            while(true) {
+                BlockRecord br;
+                try {
+                    br = bce.UVBqueue.take();
+                    System.out.println("Processing new UVB from queue");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+
+                if(bce.BC.checkPreviousHashMatch(br)) {
+                    bce.doWork2(bce.difficulty, br, false);
+                    if (bce.BC.addRecord(br)) {
+                        bce.broadcastBC();
+                    }
+                } else {
+                    System.out.println("Discarding UVB, hash doesn't match previous hash");
+                }
+            }
+        }
+
+        public void run(){
+            listen();
+        }
+
+
+    }
+
 
     class GenericListener implements Runnable{
         // listen for a new connection. If one is received hand of the socket
@@ -949,7 +1088,11 @@ class BCExecutor{
                         new Thread(new BCWorker(sock, this.bce)).start();
                     }
                 } else if (worker_type == 1){
-                    System.out.println("Nothing here " + worker_type);
+                    System.out.println("Starting UVB Listener on port: " + servsock.getLocalPort());
+                    while (true) {
+                        sock = servsock.accept();
+                        new Thread(new UVBWorker(sock, this.bce)).start();
+                    }
                 } else if (worker_type == 2){
                     System.out.println("Starting Key Listener on port: " + servsock.getLocalPort());
                     while (true) {
@@ -1090,11 +1233,96 @@ class BCExecutor{
 
             BCstruct newBC = IOHelper.getObjectFromJSON(request.toString(), BCstruct.class);
 
+
             if(newBC.getLength() > this.bce.BC.getLength()){
                 this.bce.BC = newBC;
                 System.out.println("Received updated BC: Replacing local BC");
             } else {
                 System.out.println("Received updated BC: Rejecting it as being older");
+            }
+
+
+
+        }
+    }
+    static class UVBWorker implements Runnable {
+
+        Socket sock;
+        BCExecutor bce;
+
+        // constructor: requires a socket and listener object to instantiate this object.
+        UVBWorker(Socket sock, BCExecutor listener) {
+            this.sock = sock;
+            this.bce = listener;
+        }
+
+        public void run() {
+
+            //PrintStream out;
+            BufferedReader in;
+
+            StringBuffer request; //the raw input
+
+            try {
+                //out = new PrintStream(sock.getOutputStream());
+                in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+                request = new StringBuffer();
+
+                int br = 0;
+
+                String textFromServer;
+                // Read a single line, then enter a while loop to received the entire data
+                textFromServer = in.readLine();
+                while (textFromServer != null && br < 1000) {
+                    //System.out.println(textFromServer);
+                    if (textFromServer.isEmpty()) {
+                        break;
+                    }
+                    request.append(textFromServer);
+                    br++;
+                    // read a new line prior to the next loop.
+                    textFromServer = in.readLine();
+                }
+            } catch (IOException x) {
+                System.out.println("Error: Connetion reset. Listening again..." + "\n" + x.getMessage());
+                return;
+            }
+
+            //System.out.println(request.toString());
+
+            EncodedUVBStruct newUVB = IOHelper.getObjectFromJSON(request.toString(), EncodedUVBStruct.class);
+            int rcvpid;
+            try {
+                rcvpid = Integer.parseInt(newUVB.pid);
+                if (rcvpid != 0 && rcvpid != 1 && rcvpid != 2 ) {
+                    System.out.println("ERROR: Received PID is not valid: " + newUVB.pid);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("ERROR: Received PID is not valid: " + newUVB.pid);
+                return;
+            }
+
+            byte[] newsig;
+            try {
+                newsig = SecurityHelper.base64DecodeString(newUVB.encodedSig);
+            } catch (Exception e){
+                System.out.println("Error: failed to retrieve signed UVB from network");
+                e.printStackTrace();
+                return;
+            }
+            try {
+                if(SecurityHelper.verifySignedData(bce.neighs[rcvpid].pubKey, newsig, newUVB.jsonUVB.getBytes(StandardCharsets.UTF_8))){
+                    System.out.println("Received valid UVB block, adding to priority queue. From PID" + rcvpid);
+                    BlockRecord newbr = IOHelper.getObjectFromJSON(newUVB.jsonUVB, BlockRecord.class);
+                    bce.UVBqueue.add(newbr);
+                } else {
+                    System.out.println("Signature of UVB block invalid, from PID " + rcvpid);
+                }
+            } catch (Exception e){
+                System.out.println("Error: exception while verifying or adding singed UVB from network");
+                e.printStackTrace();
+                return;
             }
 
 
